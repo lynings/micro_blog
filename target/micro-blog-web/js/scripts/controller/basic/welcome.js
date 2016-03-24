31eac6f904d1c35824e610857237f98c35c60146
Blink <yuningli@locision.com>
2016-03-24 17:56:58 +0800

    删除临时文件

--- target/micro-blog-web/js/scripts/controller/basic/welcome.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-define(['app','service/basic/welcome'], function (app) {
-    app.controller('WelcomeCtrl', WelcomeCtrl);
-});
-function WelcomeCtrl($scope,WelcomeService) {
-    WelcomeService.username().success(function(data){
-        $scope.username = data.username;
-    });
-}
